17e58840d471409c91abf01f26c933315191475c
roma1998a <106657286+roma1998a@users.noreply.github.com>
2026-06-20 19:55:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const licenseRoutes = require('./routes/license');
+
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/api/license', licenseRoutes);
 
 app.get('/', (req,res)=>{
   res.send('License Panel Running 🚀');
