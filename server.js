92cb9118aebee279c7369d386ed5b90bc1f5431a
roma1998a <106657286+roma1998a@users.noreply.github.com>
2026-06-20 20:04:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
-
-
 const app = express();
-app.use(bodyParser.json());
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
 
+// Routes
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/license', require('./routes/license'));
+
+// Pages
 app.get('/', (req,res)=>{
-  res.send('License Panel Running 🚀');
+    res.sendFile(path.join(__dirname,'public/login.html'));
+});
+
+app.get('/dashboard', (req,res)=>{
+    res.sendFile(path.join(__dirname,'public/dashboard.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log('Running on '+PORT));
+app.listen(PORT, ()=> console.log('Running on ' + PORT));
